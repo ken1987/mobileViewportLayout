b8990870d5da3c426a7e9bd67452814408ec223d
ken <ken198701@gmail.com>
2016-06-03 15:00:47 +0800

    Update mobileViewportLayout.js

--- mobileViewportLayout.js
+++ mobileViewportLayout.js
@@ -1,1 +1,59 @@
+/**
+ * 基于Viewport属性的移动WEB弹性布局解决方案
+ * @author ken
+ * @param  {number} baseSize 原始尺寸
+ * @param  {number} maxSize  最大尺寸
+ */
+var mobileViewportLayout = function(baseSize, maxSize) {
+    baseSize = parseFloat(baseSize) || 640;
+    maxSize = parseFloat(maxSize);
 
+    //默认值
+    var win = window;
+    var doc = win.document;
+
+    //检查是否存在viewport
+    var el = doc.querySelector("meta[name=viewport]");
+
+    //获取scale
+    var getScale = function() {
+        var screenSize = win.screen.width;
+        screenSize = screenSize > maxSize ? maxSize : screenSize;
+        return screenSize / baseSize;
+    };
+
+    //设置viewport
+    var setViewport = function() {
+        var scale = getScale();
+        el.setAttribute('content',
+            'initial-scale=' + scale +
+            ', maximum-scale=' + scale +
+            ', minimum-scale=' + scale +
+            ', user-scalable=no');
+    };
+
+    //绑定resize事件
+    var timer;
+    win.addEventListener('resize', function() {
+        clearTimeout(timer);
+        timer = setTimeout(setViewport, 300);
+    }, false);
+
+    //初始化
+    if (!el) {
+        el = doc.createElement('meta');
+        el.setAttribute('name', 'viewport');
+
+        setViewport();
+
+        if (doc.documentElement.firstElementChild) {
+            doc.documentElement.firstElementChild.appendChild(el);
+        } else {
+            var wrap = doc.createElement('div');
+            wrap.appendChild(el);
+            doc.write(wrap.innerHTML);
+        }
+    } else {
+        setViewport();
+    }
+};
